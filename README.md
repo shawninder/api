275dea6bd5fdf491dbd0904371fa6136e8163497
Shawn Inder <shawninder@gmail.com>
2018-09-22 15:43:00 -0400

    /feedback

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # api
 API for gaining cloud privileges
 
-- deployments
+- list deployments
+- delete deployment
+- send feedback
+
+## Usage
+
+Provide credentials via the following environment variables
+
+- ZEIT_API_TOKEN
+- GMAIL_USER
+- GMAIL_PASS
+- GMAIL_ADDRESS
--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 const url = require('url')
+const { json, send } = require('micro')
 const fetch = require('node-fetch')
 const qs = require('qs')
+const mailer = require('nodemailer')
+
+// console.log('process.env.GMAIL_USER', process.env.GMAIL_USER)
+// console.log('process.env.GMAIL_PASS', process.env.GMAIL_PASS)
+
+const smtpTransport = mailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_APP_PASS
+  }
+})
 
 module.exports = async (req, res) => {
   const uri = url.parse(req.url)
+  // console.log('uri', uri)
   const query = qs.parse(uri.query)
   const token = process.env.ZEIT_API_TOKEN
-  if (query.delete) {
+  if (uri.pathname === '/feedback' && req.method === 'POST') {
+    // console.log('POST')
+    const data = await json(req)
+    // console.log('data', data)
+    const mail = {
+      from: data.email || process.env.GMAIL_ADDRESS,
+      to: process.env.GMAIL_ADDRESS,
+      subject: `FEEDBACK - ${data.type}`,
+      text: `from: ${data.email}\n${data.body}`
+    }
+    smtpTransport.sendMail(mail, (err, response) => {
+      if (err) {
+        console.error('Error sending e-mail', err)
+        send(res, 500, { err })
+      } else {
+        // console.log('E-mail sent', response.message)
+        res.end('ok')
+      }
+      smtpTransport.close()
+    })
+  } else if (query.delete) {
     const response = await fetch(`https://api.zeit.co/v2/now/deployments/${query.delete}`, {
       method: 'DELETE',
       headers: {
